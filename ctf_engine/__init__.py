79126b0c071a8f26f495d553f0487ad160817459
Joshua Cantrell <joshuacantrell@gmail.com>
2019-08-30 14:49:05 -0500

    Updates

--- ctf_engine/__init__.py
+++ ctf_engine/__init__.py
@@ -1,5 +1,18 @@
 from flask import Flask
+from flask import render_template
 
-app = Flask(__name__, instance_relative_config=True)
-app.config.from_object("config") 
+app = Flask(__name__)
+#app.config.from_object("config") 
 app.config.from_pyfile("config.py")
+
+
+@app.route('/')
+def index():
+	return render_template('index.html')
+	
+@app.route('/boomer')
+def boomer():
+	return render_template("boomer.html")
+	
+if __name__ == '__main__':
+    app.run(debug=True)
