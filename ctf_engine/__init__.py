e64f195f9acf2a7815fc479314c06cfff77cc6ae
Joshua Cantrell <joshuacantrell@gmail.com>
2019-09-03 19:36:31 -0500

    Updates

--- ctf_engine/__init__.py
+++ ctf_engine/__init__.py
@@ -1,10 +1,10 @@
 from flask import Flask
 from flask import render_template
+from forms import LoginForm
 
 app = Flask(__name__)
 #app.config.from_object("config") 
 app.config.from_pyfile("config.py")
-
 
 @app.route('/')
 def index():
@@ -13,6 +13,11 @@ def index():
 @app.route('/boomer')
 def boomer():
 	return render_template("boomer.html")
+	
+@app.route('/login', methods=["GET", "POST"])
+def login():
+	form = LoginForm()
+	return render_template("login.html", title="Sign in", form=form)
 	
 if __name__ == '__main__':
     app.run(debug=True)
